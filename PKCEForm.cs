11d95502503ddf4739c805cf67b82bf1e244cf91
Adam E Nagy <adam_e_nagy@hotmail.com>
2024-03-15 18:23:32 +0000

    Remove unused GenerateNonce()

--- PKCEForm.cs
+++ PKCEForm.cs
@@ -136,19 +136,6 @@ namespace PKCEForm
 			listener.Stop();
 		}
 
-		private static string GenerateNonce()
-		{
-			const string chars = "abcdefghijklmnopqrstuvwxyz123456789";
-			var random = new Random();
-			var nonce = new char[128];
-			for (int i = 0; i < nonce.Length; i++)
-			{
-				nonce[i] = chars[random.Next(chars.Length)];
-			}
-
-			return new string(nonce);
-		}
-
 		public static string RandomString(int length)
 		{
 			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
